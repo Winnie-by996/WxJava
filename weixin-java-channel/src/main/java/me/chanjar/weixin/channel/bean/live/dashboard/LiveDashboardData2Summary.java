72b358bd68e11b3a7398698fbfba7b3cbe8d6857
Winnie <Lee_wei94@163.com>
2024-09-10 11:39:47 +0800

    Update LiveDashboardData2Summary.java
    修改javadoc中 author标签错误信息

--- weixin-java-channel/src/main/java/me/chanjar/weixin/channel/bean/live/dashboard/LiveDashboardData2Summary.java
+++ weixin-java-channel/src/main/java/me/chanjar/weixin/channel/bean/live/dashboard/LiveDashboardData2Summary.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
 /**
  * 直播基础数据
  *
- * @author @author <a href="https://github.com/Winnie-by996">Winnie</a>
+ * @author <a href="https://github.com/Winnie-by996">Winnie</a>
  */
 @Data
 @NoArgsConstructor
